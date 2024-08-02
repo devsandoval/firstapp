4ba9018d03e900ee52b9cd001a71fc2c0fcde643
Cesar Sandoval <cesarsandoval.dev@gmail.com>
2024-08-01 21:49:07 -0400

    feat second activity + pacman

--- app/src/main/java/com/cesarsandoval/firstapp/SecondActivity.kt
+++ app/src/main/java/com/cesarsandoval/firstapp/SecondActivity.kt
@@ -1,37 +1,43 @@
 package com.cesarsandoval.firstapp
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_second)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        var boton_enviar: Button = findViewById(R.id.button_enviar)
-        boton_enviar.setOnClickListener(View.OnClickListener {
-            Toast.makeText(this, "Bienvenido", Toast.LENGTH_LONG).show()
-        })
+        //pacman
+        val pc: ImageView = findViewById(R.id.pc)
+        val po: ImageView = findViewById(R.id.po)
+        po.setOnClickListener {
+            pc.visibility = if (pc.visibility == View.VISIBLE) View.INVISIBLE else View.VISIBLE
+        }
+        pc.setOnClickListener {
+            po.visibility = if (po.visibility == View.VISIBLE) View.INVISIBLE else View.VISIBLE
+        }
 
-        var next_img: ImageView = findViewById(R.id.next_img)
-        next_img.setOnClickListener(View.OnClickListener {
-            val intent = Intent(this, SecondActivity::class.java)
+        //gate
+        var gate: ImageButton = findViewById(R.id.gate)
+        gate.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         })
-    }
 
+    }
 }
